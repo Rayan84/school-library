207eb06fef02af2cac9654785db9967bb81b4296
Rayan84 <rayan_84@live.com>
2022-01-04 23:46:46 +0100

    Add initializers

---
+++ person.rb
@@ -0,0 +1,20 @@
+class Person
+  def initialize(name = "unknown", age, parent_permission = true)
+    @id = id
+    @name = name
+    @age = age
+  end
+
+  def getId
+    get @id
+  end
+
+  def getName
+    get @name
+  end
+
+  def getAge
+    get @
+  end
+  
+end
---
+++ teacher.rb
@@ -0,0 +1,11 @@
+class Teacher
+  
+  def initialize(name = null, age, parent_permission = true)
+    @id = id
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+
+  
+end
