1bdb0cac90b867c0ab774862ca83d2253044013d
Rayan84 <rayan_84@live.com>
2022-01-06 00:55:15 +0100

    Add getters and setters

--- person.rb
+++ person.rb
@@ -1,20 +1,44 @@
 class Person
-  def initialize(name = "unknown", age, parent_permission = true)
-    @id = id
+  def initialize(age, name = "Unknown", parent_permission = true)
     @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+
+  def id
+    @id
+  end
+
+  def name
+    @name
+  end
+
+  def age
+    @age
+  end
+
+  def setName=(name)
+    @name = name
+  end
+
+  def setAge=(age)
     @age = age
   end
 
-  def getId
-    get @id
+  def is_of_age?
+    if @age < 18
+      return false
+    else
+      return true
+    end
   end
 
-  def getName
-    get @name
+  def can_use_services?
+    if is_of_age? || @parent_permission
+       puts 'true'
+    else
+      puts 'false'
+    end
   end
-
-  def getAge
-    get @
-  end
-  
 end
+
--- student.rb
+++ student.rb
@@ -1,3 +1,2 @@
 class Student
-  
 end
