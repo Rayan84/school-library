d9199e82febc2dde4d959f1830ff8a1918ba6288
Rayan84 <rayan_84@live.com>
2022-01-06 12:10:16 +0100

    Add initilizer and play_hooky method

--- person.rb
+++ person.rb
@@ -1,5 +1,5 @@
 class Person
-  def initialize(age, name = "Unknown", parent_permission = true)
+  def initialize(age, name = "Unknown", parent_permission = 'true')
     @name = name
     @age = age
     @parent_permission = parent_permission
@@ -41,4 +41,5 @@ class Person
     end
   end
 end
+
 
--- student.rb
+++ student.rb
@@ -1,2 +1,25 @@
-class Student
+require_relative 'person.rb'
+
+class Student < Person
+  attr_reader :classrom
+  
+  def initialize(classroom, age, name = 'unknown', parent_permission = 'true')
+    super
+    @classroom = classroom
+  end
+
+  def play_hooky
+    return "¯\(ツ)/¯"
+  end
+
+
+
+  puts 'student here!'
 end
+
+
+student = Student.new(12)
+student.setName=("Adam")
+
+puts student.name
+puts student.is_of_age?
